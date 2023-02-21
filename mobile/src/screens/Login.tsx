8abb73dbee529205841980b400ae2a80db6b3413
Tijana Jung <tloochich@icloud.com>
2023-02-21 14:43:34 -0500

    login page styled properly

--- mobile/src/screens/Login.tsx
+++ mobile/src/screens/Login.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import { Text, View } from 'react-native';
 
-const LoginScreen = () => {
+
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native"
+import { useState } from "react"
+import { BlackText, Budding, LongButton, ButtonText, Input, Label, Logo, LinkText, SmallText } from '../styles/elements'
+import { Centered, CenteredCol, FlexCol, FlexRow, RowEnd, Spaced } from '../styles/containers'
+
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <View>
-            <Text> LoginScreen </Text>
-        </View>
-    );
+        <SafeAreaView>
+            <CenteredCol>
+                <FlexCol>
+                    <Spaced>
+                        <Centered>
+                            <Logo 
+                                source={require('../../assets/flower-icon.png')} />
+                            <Budding>Budding</Budding>
+                        </Centered>
+                        <Centered>
+                            <SmallText>Explore your community together.</SmallText>
+                        </Centered>
+                    </Spaced>
+                    <Spaced>
+                        <Label>Username</Label>
+                        <Input
+                            placeholder='username'
+                            value={username}
+                            onChangeText={setUsername}
+                        />
+                        <Label>Password</Label>
+                        <Input
+                            onChangeText={setPassword}
+                            placeholder='Password'
+                            secureTextEntry
+                            value={password}
+                        />
+                        <LongButton color={password.length > 8 && username.length > 3 ? '#425547' : '#9CADA4'}>
+                            <ButtonText>Login</ButtonText>
+                        </LongButton>
+                        <RowEnd>
+                            <LinkText>Forgot Password?</LinkText>
+                        </RowEnd>
+                    </Spaced>
+                    <Spaced>
+                        <FlexRow>
+                            <BlackText>Not registered yet?</BlackText>
+                            <LinkText>Create an account</LinkText>
+                        </FlexRow>
+                    </Spaced>
+                </FlexCol>
+            </CenteredCol>
+        </SafeAreaView>
+    )
 }
-
-
-export default LoginScreen;
+export default Login;
--- mobile/src/styles/global.ts
+++ mobile/src/styles/global.ts
@@ -1,9 +1,10 @@
 export default {
     colors:{
-        primary: '#425547'
+        primary: '#425547',
+        linkGreen: '#487855',
+        grey: "#9CADA4"
     },
     fontFamily:{
         primary: "Barlow-SemiBold"
     }
-
 }
